35c9ee368c9b7cc39b118827e8ec4c358b3a26ce
becky nayere <rebecca.nayere@gmail.com>
2020-08-24 21:39:21 +0300

    add a service component

---
+++ src/app/quote-box/quote-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-box',
+  templateUrl: './quote-box.component.html',
+  styleUrls: ['./quote-box.component.css']
+})
+export class QuoteBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
