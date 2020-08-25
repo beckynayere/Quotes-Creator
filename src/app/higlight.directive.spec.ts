6c4ae3efae40b9cba9a4969df7b65a82abebaab1
becky nayere <rebecca.nayere@gmail.com>
2020-08-26 00:54:20 +0300

    add image to  component

--- src/app/higlight.directive.spec.ts
+++ src/app/higlight.directive.spec.ts
@@ -1,10 +1,18 @@
-import { HighlightDirective } from './higlight.directive';
+// import { HighlightDirective } from './higlight.directive';
 
 
-describe('HiglightDirective', () => {
+// describe('HiglightDirective', () => {
+//   it('should create an instance', () => {
+//     const directive = new HighlightDirective();
+//     expect(directive).toBeTruthy();
+//   });
+// });
+
+import { HighlightDirective } from '. /highlight.directive';
+
+describe('HighlightDirective', () => {
   it('should create an instance', () => {
     const directive = new HighlightDirective();
     expect(directive).toBeTruthy();
   });
 });
-
